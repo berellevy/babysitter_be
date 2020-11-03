524ba994d3c797116f8be11ee69d50ad42f590e3
berellevy <berellevy@gmail.com>
2020-11-03 15:48:45 -0500

    added root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  def home
+    render json: {message: "hello"}
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root to: "application#home"
   namespace :api do
     namespace :v1 do
       resources :sitters
+     
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
