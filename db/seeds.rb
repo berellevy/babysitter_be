4bc7e1a9f512d10c8174753ca4e8974046c4728e
berellevy <berellevy@gmail.com>
2020-11-03 15:54:10 -0500

    added 2 new sitters

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Sitter.create(
-  name: "berel",
-  birthday: "1989-08-19",
-  years_of_experience: 2,
-  bio: "I'm a person",
-  contact_name: "Flumtzah",
-  contact_phone: "9176798518"
+  name: "Johnny",
+  birthday: "1989-08-03",
+  years_of_experience: 3,
+  bio: "I'm another person",
+  contact_name: "H. G. Rickesmacker",
+  contact_phone: "9176587897"
+)
+
+Sitter.create(
+  name: "Bart",
+  birthday: "1995-08-03",
+  years_of_experience: 1,
+  bio: "I'm a yellow person",
+  contact_name: "Marge Simpson",
+  contact_phone: "9178387897"
 )
