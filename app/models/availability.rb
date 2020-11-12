498ba153aa3fa9577fe118ba770ef91c829b376d
berellevy <berellevy@gmail.com>
2020-11-12 16:58:36 -0500

    don't need min period validator anymore

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -4,5 +4,4 @@ class Availability < ApplicationRecord
   # TODO: validate uniqueness of all fields together
   validates :weekday, inclusion: {in: 0..6, message: "Must be an int from zero to six"}
   validates :start_time, presence: true
-  # validates_with MinPeriodValidator
 end
