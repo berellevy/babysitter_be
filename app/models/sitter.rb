7370e79c08fe895f321b8c995006d4b05bfb4fc3
berellevy <berellevy@gmail.com>
2020-11-18 16:46:17 -0500

    convert query to work with duration

--- app/models/sitter.rb
+++ app/models/sitter.rb
@@ -13,30 +13,47 @@ class Sitter < ApplicationRecord
   def with_calc
       {
         **serializable_hash.symbolize_keys, 
-        age: age,
-        references: references,
-        availabilities: availabilities,
-        appointments: appointments
+        age: age
       }
+  end
+
+  def with_relations
+    {
+      **with_calc, 
+      references: references,
+      availabilities: availabilities,
+      appointments: appointments
+    }
   end
 
 
   ## CLASS METHODS
 
-  def self.find_by_availabilities(params)
-    day, start_time, duration = params.values_at("day", "startTime", "duration")
+    def self.find_by_availabilities(params)
+      day, start_time, duration = params.values_at("day", "startTime", "duration")
 
-    day = Date.parse(params["day"]).cwday
+      weekday = Date.parse(day).cwday
+      puts "weekday", weekday
 
-    query_text = %{
-    sitters.available = true AND
-    availabilities.weekday = ? AND
-    availabilities.start_time <= ? AND
-    availabilities.end_time >= ?
-    }
-    group = "sitters.id"
-    select = "sitters.*"
-    joins(:availabilities).where(query_text, day, from, to).group(group).select(select)
+      join_clause = <<-SQL 
+        INNER JOIN (
+          SELECT 
+            availabilities.*, 
+            (EXTRACT(epoch FROM \'#{start_time}\' - availabilities.start_time) / 60) AS query_start_minus_record_start,  
+            (\'#{duration}\' - availabilities.duration_minutes) AS query_duration_minus_record_duration   
+            FROM "availabilities"
+          ) AS a
+        ON sitters.id = a.sitter_id
+      SQL
+
+      where_clause = <<-SQL
+          weekday = #{weekday} AND
+          a.query_start_minus_record_start >= 0 AND
+          a.query_duration_minus_record_duration <= 0 AND
+          a.query_start_minus_record_start + a.query_duration_minus_record_duration <= 0
+      SQL
+
+    joins(join_clause).where(where_clause).group("sitters.id")
   end
 
   def self.with_calc
