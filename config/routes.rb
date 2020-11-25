c1537d3b9ffaea439b12d0193d924c734a4aba0a
berellevy <berellevy@gmail.com>
2020-11-25 10:50:31 -0500

    small changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
-  root to: "application#home"
+  
+  # root to: "application#home"
   namespace :api do
     namespace :v1 do
       resources :sitters
      
     end
   end
+  get "/:suffix", to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
