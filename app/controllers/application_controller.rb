5e6a5d7a1352a7c1d72e8c5fd4a8c9f8aa428d1b
berellevy <berellevy@gmail.com>
2020-11-25 10:26:34 -0500

    convert to reg rails app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   include ::ActionView::Layouts
   def home
     render json: {message: "hello"}
