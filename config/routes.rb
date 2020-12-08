9fb80fc7ba8918d64e6fd5cfd04e8d1b7c340759
berellevy <berellevy@gmail.com>
2020-12-08 11:55:50 -0500

    questionable changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  post 'auth/request', to:'authorization#get_authorization'
   root to: "application#home"
   namespace :api do
     namespace :v1 do
       resources :sitters
+      post 'auth/request', to:'authorization#create_user_from_auth'
+      post 'auth/login', to: 'authorization#login'
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
      
     end
   end
