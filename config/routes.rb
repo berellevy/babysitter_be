0b8ad1c287d3519c9c6ef1448cf74868cb2a0855
berellevy <berellevy@gmail.com>
2020-12-08 17:20:55 -0500

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
-  post 'auth/request', to:'authorization#get_authorization'
-  root to: "application#home"
   namespace :api do
     namespace :v1 do
-      resources :sitters
-      post 'auth/request', to:'authorization#create_user_from_auth'
+      get "sitters", to: "public#index"
+      get "sitters/:id", to: "public#show"
       post 'auth/login', to: 'authorization#login'
-      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
-     
+      get 'sitter', to: "sitters#show"
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
