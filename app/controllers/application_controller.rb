4688f3c33cff335dfcad028d29a07c348e86e0dc
berellevy <berellevy@gmail.com>
2020-12-08 11:51:50 -0500

    remove take out home page and start with auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
+require 'httparty'
 class ApplicationController < ActionController::API
-  def home
-    render json: {message: "hello"}
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include HTTParty
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def id_token
+    auth_header.split(" ")[1]
   end
   
 end
