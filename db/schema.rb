4bdd9808855ae1eb8ce8a429c4eb1786f12a3e2b
berellevy <berellevy@gmail.com>
2020-12-10 12:01:31 -0500

    remove users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_183259) do
+ActiveRecord::Schema.define(version: 2020_12_10_165955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,45 +70,6 @@ ActiveRecord::Schema.define(version: 2020_12_08_183259) do
     t.datetime "access_token_date", null: false
     t.index ["access_token"], name: "index_sitters_on_access_token"
     t.index ["uid"], name: "index_sitters_on_uid"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "name"
-    t.string "nickname"
-    t.string "image"
-    t.string "email"
-    t.string "first_name"
-    t.datetime "birthday"
-    t.integer "years_of_experience"
-    t.text "bio"
-    t.string "contact_name"
-    t.string "contact_phone"
-    t.float "price"
-    t.string "contact_relation"
-    t.string "last_name"
-    t.boolean "available", default: true, null: false
-    t.string "google_token"
-    t.string "google_refresh_token"
-    t.json "tokens"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "access_token"
-    t.datetime "access_token_date"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "appointments", "sitters"
