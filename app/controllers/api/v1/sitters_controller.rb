ef8da3608e1a86f8450e8c4d9a483c5fc22aef5f
berellevy <berellevy@gmail.com>
2020-12-10 11:54:18 -0500

    add appts and new appts

--- app/controllers/api/v1/sitters_controller.rb
+++ app/controllers/api/v1/sitters_controller.rb
@@ -4,4 +4,29 @@ class Api::V1::SittersController < ProtectedController
     render json: current_sitter
   end
 
+  def appointments
+    render json: current_sitter.appointments
+  end
+
+  def new_appointment
+    appointment = current_sitter.appointments.new(
+      start_time: appointment_params[:startDate],
+      duration_minutes: appointment_params[:selection],
+      contact_name: appointment_params[:name],
+      contact_phone: appointment_params[:phone]
+    )
+    if appointment.save
+      pp appointment
+      render json: appointment, status: :ok
+    else
+      render json: {errors: appointment.errors}, status: 404
+    end
+  end
+  
+  private
+
+  def appointment_params
+    params.permit(:startDate, :selection, :phone, :name)
+  end
+
 end
