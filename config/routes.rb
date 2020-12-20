e042a71d7cb2c323cb13f7ee2c027c776d01b50d
berellevy <berellevy@gmail.com>
2020-12-20 12:07:48 -0500

    add availabilities index and get and set is availa

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,13 @@ Rails.application.routes.draw do
       post 'appointments', to: "sitters#new_appointment"
       get "appointments", to: "sitters#appointments"
       patch "update_sitter", to: "sitters#update_sitter"
+      get "is-available", to: "sitters#get_is_available"
+      post "set-is-available", to: "sitters#set_is_available"
       get "references", to: "sitters#references"
       post "references", to: "sitters#add_reference"
       patch "references", to: "sitters#update_reference"
       delete "references", to: "sitters#delete_reference"
+      resources :availabilities, only: [:index]
     end
   end
 end
